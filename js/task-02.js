cf8f5363d4f34bab895e9c2339fb2a2e2f9668f2
Anastasiia Yavtushenko <Icenn85@gmail.com>
2022-08-03 18:12:46 +0300

    add task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredListEl = document.querySelector("#ingredients");
+
+const ingredItemEl = ingredients.map((ingredients) => {
+  const createItem = document.createElement("li");
+  createItem.textContent = ingredients;
+  createItem.classList.add("item");
+  return createItem;
+});
+
+ingredListEl.append(...ingredItemEl);
+
+console.log(ingredListEl);
+
